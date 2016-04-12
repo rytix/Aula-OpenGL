c521f3e32471368a3a92b843a42c3afababc7a07
rytix <aff12300@gmail.com>
2016-04-12 16:03:32 -0300

    Alocações dinamicas...

--- Invader/main.cpp
+++ Invader/main.cpp
@@ -3,69 +3,109 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <list>
 
-//Atributos da nave
-//*Constantes
+using namespace std;
+// *** Definição da nave ***
+//Constantes
 const GLfloat H_NAVE = 30.0f;
 const GLfloat W_NAVE = 20.0f;
 const GLfloat X_STEP_NAVE = 5.0f;
-//*Variaveis
+//Variaveis
 struct Nave{
     GLfloat x;
     GLfloat y;
 };
-//Atributos do Alien
-//*Constantes
-    const GLfloat W_ALIEN = 25.0f;
-    const GLfloat H_ALIEN = 15.0f;
-    const GLfloat ESPACAMENTO = 0.0f;
-    const int FRAMES_ALIEN = 2;
-    const int LINHAS_ALIEN = 8;
-    const int COLUNAS_ALIEN = 11;
-    //Desenhos Aliens
-    //Alien Normal
-    GLfloat ALIEN_1_FRAME[FRAMES_ALIEN][LINHAS_ALIEN][COLUNAS_ALIEN] =
+// *** Definição do tiro ***
+const GLfloat largura = 1.0f;
+const GLfloat altura = 10.0f;
+struct Tiro{
+    GLfloat x;
+    GLfloat y;
+};
+// *** Definição do Alien ***
+//Constantes
+const GLfloat W_ALIEN = 25.0f;
+const GLfloat H_ALIEN = 15.0f;
+const GLfloat ESPACAMENTO = 0.0f;
+const int FRAMES_ALIEN = 2;
+const int LINHAS_ALIEN = 8;
+const int COLUNAS_ALIEN = 11;
+//Desenhos Aliens
+//Alien Normal
+GLfloat ALIEN_1_FRAME[FRAMES_ALIEN][LINHAS_ALIEN][COLUNAS_ALIEN] =
+{
     {
-        {
-        {0,0,1,0,0,0,0,0,1,0,0},
-        {0,0,0,1,0,0,0,1,0,0,0},
-        {0,0,1,1,1,1,1,1,1,0,0},
-        {0,1,1,0,1,1,1,0,1,1,0},
-        {1,1,1,1,1,1,1,1,1,1,1},
-        {1,0,1,1,1,1,1,1,1,0,1},
-        {1,0,1,0,0,0,0,0,1,0,1},
-        {0,0,0,1,1,0,1,1,0,0,0}
-        },
-        {
-        {0,0,1,0,0,0,0,0,1,0,0},
-        {0,0,0,1,0,0,0,1,0,0,0},
-        {0,0,1,1,1,1,1,1,1,0,0},
-        {0,1,1,0,1,1,1,0,1,1,0},
-        {1,1,1,1,1,1,1,1,1,1,1},
-        {1,0,1,1,1,1,1,1,1,0,1},
-        {1,0,1,0,0,0,0,0,1,0,1},
-        {0,0,0,1,1,0,1,1,0,0,0}
-        }
-    };
-    GLfloat ALIEN_1_RGB[FRAMES_ALIEN][3] = {{0,0,0},{0,0,0}};
-//*Variaveis
+    {0,0,1,0,0,0,0,0,1,0,0},
+    {0,0,0,1,0,0,0,1,0,0,0},
+    {0,0,1,1,1,1,1,1,1,0,0},
+    {0,1,1,0,1,1,1,0,1,1,0},
+    {1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,1,1,1,1,1,1,1,0,1},
+    {1,0,1,0,0,0,0,0,1,0,1},
+    {0,0,0,1,1,0,1,1,0,0,0}
+    },
+    {
+    {0,0,1,0,0,0,0,0,1,0,0},
+    {0,0,0,1,0,0,0,1,0,0,0},
+    {0,0,1,1,1,1,1,1,1,0,0},
+    {0,1,1,0,1,1,1,0,1,1,0},
+    {1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,1,1,1,1,1,1,1,0,1},
+    {1,0,1,0,0,0,0,0,1,0,1},
+    {0,0,0,1,1,0,1,1,0,0,0}
+    }
+};
+GLfloat ALIEN_1_RGB[FRAMES_ALIEN][3] = {{0,0,0},{0,0,0}};
+//Variaveis
 struct Alien{
     GLfloat x;
     GLfloat y;
     GLfloat frame[FRAMES_ALIEN][LINHAS_ALIEN][COLUNAS_ALIEN];
     GLfloat rgb[FRAMES_ALIEN][3];
 };
-
+// *** FIM DE DEFINIÇÕES ***
+// *** OPENGL E CABEÇALHO ***
 // Largura e altura da janela
 GLfloat windowWidth;
 GLfloat windowHeight;
 //Cabeçalho
 void copiar_matriz_frames(GLfloat origem[FRAMES_ALIEN][LINHAS_ALIEN][COLUNAS_ALIEN], GLfloat destino[FRAMES_ALIEN][LINHAS_ALIEN][COLUNAS_ALIEN]);
 void copiar_matriz_rgb(GLfloat origem[FRAMES_ALIEN][3],GLfloat destino[FRAMES_ALIEN][3]);
-//Variaveis Globais
+void desenhar_nave(Nave nave);
+void desenhar_alien(Alien alien);
+// *** FIM DO OPENGL E CABEÇALHO ***
+//Variaveis do Jogo
 Nave nave;
-//OpenGL
+list<Alien> aliens;
+list<Tiro> tiros;
+bool estado_reiniciar_jogo = true;
+//Funções do jogo
+void reiniciar_jogo(){
+    if(estado_reiniciar_jogo){
+        estado_reiniciar_jogo = false;
+        nave.x = 100;
+        nave.y = 20;
+        for(int i = 0; i < 5; i++){
+            Alien *alien;
+            alien = new Alien;
+            alien->x = i*60 + 60;
+            alien->y = 120;
+            copiar_matriz_frames(ALIEN_1_FRAME,alien->frame);
+            copiar_matriz_rgb(ALIEN_1_RGB,alien->rgb);
+            aliens.push_back(*alien);
+        }
+    }
+}
+//*** OpenGL ***
 //Desenhos
+void desenhar_tudo(){
+    desenhar_nave(nave);
+    list<Alien>::iterator it = aliens.begin();
+    while(it != aliens.end()){
+        desenhar_alien(*it++);
+    }
+}
 void desenhar_nave(Nave nave){
     glColor3f(0.5f, 0.0f, 1.0f);
     glBegin(GL_TRIANGLES);
@@ -90,7 +130,14 @@ void desenhar_alien(Alien alien){
         }
     }
 }
-
+//Definições do OPENGL
+void Timer(int value) //"Main Loop"
+{
+    reiniciar_jogo();
+    // Redesenha o quadrado com as novas coordenadas
+    glutPostRedisplay();
+    glutTimerFunc(33,Timer, 1);
+}
 //Função callback chamada para fazer o desenho
 void LerTeclado(unsigned char key, int x, int y){
     switch (key) {
@@ -114,34 +161,16 @@ void Desenha(void)
 {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
     // Limpa a janela de visualização com a cor de fundo especificada
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Especifica que a cor corrente é vermelha
-    //         R     G     B
-
-    //Desenha a nave
-    glColor3f(0.5f, 1.0f, 0.0f);
-    //desenha os aliens (provavel que precise de uma lista também)
-    Alien alien;
-    copiar_matriz_frames(ALIEN_1_FRAME,alien.frame);
-    copiar_matriz_rgb(ALIEN_1_RGB,alien.rgb);
-    alien.x = 50;
-    alien.y = 50;
-    desenhar_alien(alien);
+    desenhar_tudo();
     // Executa os comandos OpenGL
     glutSwapBuffers();
 }
 
 // Função callback chamada pela GLUT a cada intervalo de tempo
 // (a window não está sendo redimensionada ou movida)
-void Timer(int value)
-{
-    // Redesenha o quadrado com as novas coordenadas
-    glutPostRedisplay();
-    glutTimerFunc(33,Timer, 1);
-}
 
 // Inicializa parâmetros de rendering
 void Inicializa (void)
